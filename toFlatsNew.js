43b4cb90485e04a526ad3c7189fd50306ba93e4d
Jessie Hong <NeoMvsEu@gmail.com>
2017-10-30 18:41:49 -0400

    Expansion
    added isContentEditable boolean changes and 6 chords

--- toFlatsNew.js
+++ toFlatsNew.js
@@ -3,86 +3,333 @@ for (var i = 0; i < chordEls.length; i++)
 {
 	switch(chordEls[i].innerHTML) {
 		case "F#/A#":
+			chordEls[i].isContentEditable = true;
 			chordEls[i].innerHTML = "Gb/Bb";
+			chordEls[i].isContentEditable = false;
 			break;
 		case "A#m7":
+			chordEls[i].isContentEditable = true;
 			chordEls[i].innerHTML = "Bbm7";
+			chordEls[i].isContentEditable = false;
+			break;
+		case "A#m6":
+			chordEls[i].isContentEditable = true;
+			chordEls[i].innerHTML = "Bbm6";
+			chordEls[i].isContentEditable = false;
 			break;
 		case "A#7":
+			chordEls[i].isContentEditable = true;
 			chordEls[i].innerHTML = "Bb7";
+			chordEls[i].isContentEditable = false;
+			break;
+		case "A#6":
+			chordEls[i].isContentEditable = true;
+			chordEls[i].innerHTML = "Bb6";
+			chordEls[i].isContentEditable = false;
 			break;
 		case "A#m":
+			chordEls[i].isContentEditable = true;
 			chordEls[i].innerHTML = "Bbm";
+			chordEls[i].isContentEditable = false;
 			break;
 		case "A#":
+			chordEls[i].isContentEditable = true;
 			chordEls[i].innerHTML = "Bb";
+			chordEls[i].isContentEditable = false;
 			break;
 		
 		case "B/D#":
+			chordEls[i].isContentEditable = true;
 			chordEls[i].innerHTML = "Cb/Eb";
+			chordEls[i].isContentEditable = false;
 			break;
 		case "D#7":
+			chordEls[i].isContentEditable = true;
 			chordEls[i].innerHTML = "Eb7";
+			chordEls[i].isContentEditable = false;
+			break;
+		case "D#6":
+			chordEls[i].isContentEditable = true;
+			chordEls[i].innerHTML = "Eb6";
+			chordEls[i].isContentEditable = false;
 			break;
 		case "D#m7":
+			chordEls[i].isContentEditable = true;
 			chordEls[i].innerHTML = "Ebm7";
+			chordEls[i].isContentEditable = false;
+			break;
+		case "D#m6":
+			chordEls[i].isContentEditable = true;
+			chordEls[i].innerHTML = "Ebm6";
+			chordEls[i].isContentEditable = false;
 			break;
 		case "D#m":
+			chordEls[i].isContentEditable = true;
 			chordEls[i].innerHTML = "Ebm";
+			chordEls[i].isContentEditable = false;
 			break;
 		case "D#":
+			chordEls[i].isContentEditable = true;
 			chordEls[i].innerHTML = "Eb";
+			chordEls[i].isContentEditable = false;
 			break;
 		
 		case "E/G#":
+			chordEls[i].isContentEditable = true;
 			chordEls[i].innerHTML = "Fb/Ab";
+			chordEls[i].isContentEditable = false;
 			break;
 		case "G#m7":
+			chordEls[i].isContentEditable = true;
 			chordEls[i].innerHTML = "Abm7";
+			chordEls[i].isContentEditable = false;
 			break;
 		case "G#7":
+			chordEls[i].isContentEditable = true;
 			chordEls[i].innerHTML = "Ab7";
+			chordEls[i].isContentEditable = false;
+			break;
+		case "G#6":
+			chordEls[i].isContentEditable = true;
+			chordEls[i].innerHTML = "Ab6";
+			chordEls[i].isContentEditable = false;
 			break;
 		case "G#m":
+			chordEls[i].isContentEditable = true;
 			chordEls[i].innerHTML = "Abm";
+			chordEls[i].isContentEditable = false;
+			break;
+		case "G#m6":
+			chordEls[i].isContentEditable = true;
+			chordEls[i].innerHTML = "Abm6";
+			chordEls[i].isContentEditable = false;
 			break;
 		case "G#":
+			chordEls[i].isContentEditable = true;
 			chordEls[i].innerHTML = "Ab";
+			chordEls[i].isContentEditable = false;
 			break;
 
 		case "C#/F":
+			chordEls[i].isContentEditable = true;
 			chordEls[i].innerHTML = "Db/F";
+			chordEls[i].isContentEditable = false;
 			break;
 		case "C#/E#":
+			chordEls[i].isContentEditable = true;
 			chordEls[i].innerHTML = "Db/F";
+			chordEls[i].isContentEditable = false;
 			break;
 		case "C#m7":
+			chordEls[i].isContentEditable = true;
 			chordEls[i].innerHTML = "Dbm7";
+			chordEls[i].isContentEditable = false;
+			break;
+		case "C#m6":
+			chordEls[i].isContentEditable = true;
+			chordEls[i].innerHTML = "Dbm6";
+			chordEls[i].isContentEditable = false;
 			break;
 		case "C#7":
+			chordEls[i].isContentEditable = true;
 			chordEls[i].innerHTML = "Db7";
+			chordEls[i].isContentEditable = false;
+			break;
+		case "C#6":
+			chordEls[i].isContentEditable = true;
+			chordEls[i].innerHTML = "Db6";
+			chordEls[i].isContentEditable = false;
 			break;
 		case "C#m":
+			chordEls[i].isContentEditable = true;
 			chordEls[i].innerHTML = "Dbm";
+			chordEls[i].isContentEditable = false;
 			break;
 		case "C#":
+			chordEls[i].isContentEditable = true;
 			chordEls[i].innerHTML = "Db";
+			chordEls[i].isContentEditable = false;
 			break;
 
 		case "F#/A#":
+			chordEls[i].isContentEditable = true;
 			chordEls[i].innerHTML = "Gb/Bb";
+			chordEls[i].isContentEditable = false;
 			break;
 		case "F#m7":
+			chordEls[i].isContentEditable = true;
 			chordEls[i].innerHTML = "Gbm7";
+			chordEls[i].isContentEditable = false;
+			break;
+		case "F#m6":
+			chordEls[i].isContentEditable = true;
+			chordEls[i].innerHTML = "Gbm6";
+			chordEls[i].isContentEditable = false;
 			break;
 		case "F#7":
+			chordEls[i].isContentEditable = true;
 			chordEls[i].innerHTML = "Gb7";
+			chordEls[i].isContentEditable = false;
+			break;
+		case "F#6":
+			chordEls[i].isContentEditable = true;
+			chordEls[i].innerHTML = "Gb6";
+			chordEls[i].isContentEditable = false;
 			break;
 		case "F#m":
+			chordEls[i].isContentEditable = true;
 			chordEls[i].innerHTML = "Gbm";
+			chordEls[i].isContentEditable = false;
 			break;
 		case "F#":
+			chordEls[i].isContentEditable = true;
 			chordEls[i].innerHTML = "Gb";
+			chordEls[i].isContentEditable = false;
+			break;
+		
+		default:
+			break;
+		} //end switch
+} //end loop
+
+var diagramEls = document.getElementsByClassName("_2q3RI");
+
+for (var i = 0; i < diagramEls.length; i++)
+{
+	switch(diagramEls[i].innerHTML) {
+		case "F#/A#":
+			diagramEls[i].isContentEditable = true;
+			diagramEls[i].innerHTML = "Gb/Bb";
+			diagramEls[i].isContentEditable = false;
+			break;
+		case "A#m7":
+			diagramEls[i].isContentEditable = true;
+			diagramEls[i].innerHTML = "Bbm7";
+			diagramEls[i].isContentEditable = false;
+			break;
+		case "A#7":
+			diagramEls[i].isContentEditable = true;
+			diagramEls[i].innerHTML = "Bb7";
+			diagramEls[i].isContentEditable = false;
+			break;
+		case "A#m":
+			diagramEls[i].isContentEditable = true;
+			diagramEls[i].innerHTML = "Bbm";
+			diagramEls[i].isContentEditable = false;
+			break;
+		case "A#":
+			diagramEls[i].isContentEditable = true;
+			diagramEls[i].innerHTML = "Bb";
+			diagramEls[i].isContentEditable = false;
+			break;
+		
+		case "B/D#":
+			diagramEls[i].isContentEditable = true;
+			diagramEls[i].innerHTML = "Cb/Eb";
+			diagramEls[i].isContentEditable = false;
+			break;
+		case "D#7":
+			diagramEls[i].isContentEditable = true;
+			diagramEls[i].innerHTML = "Eb7";
+			diagramEls[i].isContentEditable = false;
+			break;
+		case "D#m7":
+			diagramEls[i].isContentEditable = true;
+			diagramEls[i].innerHTML = "Ebm7";
+			diagramEls[i].isContentEditable = false;
+			break;
+		case "D#m":
+			diagramEls[i].isContentEditable = true;
+			diagramEls[i].innerHTML = "Ebm";
+			diagramEls[i].isContentEditable = false;
+			break;
+		case "D#":
+			diagramEls[i].isContentEditable = true;
+			diagramEls[i].innerHTML = "Eb";
+			diagramEls[i].isContentEditable = false;
+			break;
+		
+		case "E/G#":
+			diagramEls[i].isContentEditable = true;
+			diagramEls[i].innerHTML = "Fb/Ab";
+			diagramEls[i].isContentEditable = false;
+			break;
+		case "G#m7":
+			diagramEls[i].isContentEditable = true;
+			diagramEls[i].innerHTML = "Abm7";
+			diagramEls[i].isContentEditable = false;
+			break;
+		case "G#7":
+			diagramEls[i].isContentEditable = true;
+			diagramEls[i].innerHTML = "Ab7";
+			diagramEls[i].isContentEditable = false;
+			break;
+		case "G#m":
+			diagramEls[i].isContentEditable = true;
+			diagramEls[i].innerHTML = "Abm";
+			diagramEls[i].isContentEditable = false;
+			break;
+		case "G#":
+			diagramEls[i].isContentEditable = true;
+			diagramEls[i].innerHTML = "Ab";
+			diagramEls[i].isContentEditable = false;
+			break;
+
+		case "C#/F":
+			diagramEls[i].isContentEditable = true;
+			diagramEls[i].innerHTML = "Db/F";
+			diagramEls[i].isContentEditable = false;
+			break;
+		case "C#/E#":
+			diagramEls[i].isContentEditable = true;
+			diagramEls[i].innerHTML = "Db/F";
+			diagramEls[i].isContentEditable = false;
+			break;
+		case "C#m7":
+			diagramEls[i].isContentEditable = true;
+			diagramEls[i].innerHTML = "Dbm7";
+			diagramEls[i].isContentEditable = false;
+			break;
+		case "C#7":
+			diagramEls[i].isContentEditable = true;
+			diagramEls[i].innerHTML = "Db7";
+			diagramEls[i].isContentEditable = false;
+			break;
+		case "C#m":
+			diagramEls[i].isContentEditable = true;
+			diagramEls[i].innerHTML = "Dbm";
+			diagramEls[i].isContentEditable = false;
+			break;
+		case "C#":
+			diagramEls[i].isContentEditable = true;
+			diagramEls[i].innerHTML = "Db";
+			diagramEls[i].isContentEditable = false;
+			break;
+
+		case "F#/A#":
+			diagramEls[i].isContentEditable = true;
+			diagramEls[i].innerHTML = "Gb/Bb";
+			diagramEls[i].isContentEditable = false;
+			break;
+		case "F#m7":
+			diagramEls[i].isContentEditable = true;
+			diagramEls[i].innerHTML = "Gbm7";
+			diagramEls[i].isContentEditable = false;
+			break;
+		case "F#7":
+			diagramEls[i].isContentEditable = true;
+			diagramEls[i].innerHTML = "Gb7";
+			diagramEls[i].isContentEditable = false;
+			break;
+		case "F#m":
+			diagramEls[i].isContentEditable = true;
+			diagramEls[i].innerHTML = "Gbm";
+			diagramEls[i].isContentEditable = false;
+			break;
+		case "F#":
+			diagramEls[i].isContentEditable = true;
+			diagramEls[i].innerHTML = "Gb";
+			diagramEls[i].isContentEditable = false;
 			break;
 		
 		default:
